4e35f4fb771bb0e37b7d8fe44b11b549e610fdc1
saraAber <53268664+saraAber@users.noreply.github.com>
2022-09-13 21:08:39 +0300

    Update Global.asax.cs

--- Server/Global.asax.cs
+++ Server/Global.asax.cs
@@ -18,6 +18,21 @@ namespace Server
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+        }
+
+        protected void Application_BeginRequest()
+        {
+            Response.AddHeader("Access-Control-Allow-Origin", "*");
+            Response.AddHeader("Access-Control-Allow-Methods", "*");
+            Response.AddHeader("Access-Control-Allow-Headers", "*");
+
+            if (Request.Headers.AllKeys.Contains("Origin", StringComparer.CurrentCultureIgnoreCase)
+                && Request.HttpMethod == "OPTIONS")
+            {
+                Response.End();
+            }
+
         }
     }
 }

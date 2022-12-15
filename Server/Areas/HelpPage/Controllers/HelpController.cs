c4e865e0bc2f8f1bc6d9ac8bee2834ad78b53a05
ortaldiamant <109242596+ortaldiamant@users.noreply.github.com>
2022-12-15 22:13:38 +0200

    Update HelpController.cs

--- Server/Areas/HelpPage/Controllers/HelpController.cs
+++ Server/Areas/HelpPage/Controllers/HelpController.cs
@@ -11,53 +11,14 @@ namespace Server.Areas.HelpPage.Controllers
     /// </summary>
     public class HelpController : Controller
     {
-        private const string ErrorViewName = "Error";
 
-        public HelpController()
-            : this(GlobalConfiguration.Configuration)
-        {
-        }
+        
 
-        public HelpController(HttpConfiguration config)
-        {
-            Configuration = config;
-        }
+       
 
-        public HttpConfiguration Configuration { get; private set; }
 
-        public ActionResult Index()
-        {
-            ViewBag.DocumentationProvider = Configuration.Services.GetDocumentationProvider();
-            return View(Configuration.Services.GetApiExplorer().ApiDescriptions);
-        }
+       
 
-        public ActionResult Api(string apiId)
-        {
-            if (!String.IsNullOrEmpty(apiId))
-            {
-                HelpPageApiModel apiModel = Configuration.GetHelpPageApiModel(apiId);
-                if (apiModel != null)
-                {
-                    return View(apiModel);
-                }
-            }
 
-            return View(ErrorViewName);
-        }
-
-        public ActionResult ResourceModel(string modelName)
-        {
-            if (!String.IsNullOrEmpty(modelName))
-            {
-                ModelDescriptionGenerator modelDescriptionGenerator = Configuration.GetModelDescriptionGenerator();
-                ModelDescription modelDescription;
-                if (modelDescriptionGenerator.GeneratedModels.TryGetValue(modelName, out modelDescription))
-                {
-                    return View(modelDescription);
-                }
-            }
-
-            return View(ErrorViewName);
-        }
     }
 }
